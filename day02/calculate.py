07dd8ce6ac7aabcd4d271bebcb284575e4df21d8
杨自立 <1574746368@qq.com>
2019-03-22 15:09:55 +0800

    测试提交

--- day02/calculate.py
+++ day02/calculate.py
@@ -1,5 +1,19 @@
+# 取余
+
+def cal(a,b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    print(a%b)
 
 if __name__ == '__main__':
-
+    a = 10
+    b = 3
+    c = 10
+    print(a>b)
+    print(a<b)
+    print(a == b)
+    print(a == c)
 
 
