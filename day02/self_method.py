7042bbf97c13081599fd5ac62ba4f5100f950375
杨自立 <1574746368@qq.com>
2019-03-22 17:15:42 +0800

    测试提交

--- day02/self_method.py
+++ day02/self_method.py
@@ -1,4 +1,5 @@
 # 步长
+# for语句
 
 def list_bianli():
     for i in range(10):
@@ -26,6 +27,38 @@ def chengfabiao():
             print('%s X %s = %s' %(i,j,i*j),end='   ')
         print(' ')
 
-if __name__ == '__main__':
-    chengfabiao()
+# 基本 if else 语法演示
+def if_demo():
+    a = False
+    if a :
+        print('a是对的')
+    else:
+        print('a是错的')
 
+# if else语法原理
+def if_demo0():
+    if False :
+        print('正确')
+    else:
+        print('错误')
+
+# 判断 a 和 b的大小
+def if_demo1():
+    a=10
+    b=20
+    if a>b:
+        print('a大于b')
+    else:
+        print('a小于b')
+
+# 输出 比较大的值
+def if_demo2():
+    a=10
+    b=20
+    if a>b:
+        print(a)
+    else:
+        print(b)
+
+if __name__ == '__main__':
+    if_demo0()
