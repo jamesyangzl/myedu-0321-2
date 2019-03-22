103a0a3223fa475a30d852c81bbcee4066e6b8cb
杨自立 <1574746368@qq.com>
2019-03-22 15:28:53 +0800

    测试提交

--- day02/calculate.py
+++ day02/calculate.py
@@ -1,19 +1,38 @@
 # 取余
 
-def cal(a,b):
+def calculate(a,b):
     print(a+b)
     print(a-b)
     print(a*b)
     print(a/b)
     print(a%b)
 
-if __name__ == '__main__':
-    a = 10
-    b = 3
-    c = 10
+def compare(a,b,c):
     print(a > b)
     print(a < b)
     print(a == b)
     print(a == c)
+
+def deng(a):
+    a+=1    #   a = a+1
+    print(a)
+    a*=2    #   a = a*2
+    print(a)
+    a-=5    #   a = a-2
+    print(a)
+    a/=2    #   a = a/2
+    print(a)
+    a = a+1  #  和a+=1是一样的结果
+    print(a)
+    a+=1
+    print(a)
+
+if __name__ == '__main__':
+    a = 10
+
+    # b = 3
+    # c = 10
+
+
 
 
