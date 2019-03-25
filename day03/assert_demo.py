309387abdc1a98c5a8477b423055d8494d152f6f
杨自立 <1574746368@qq.com>
2019-03-25 10:50:49 +0800

    0325

---
+++ day03/assert_demo.py
@@ -0,0 +1,46 @@
+
+def assert_true():      #true不会报错
+    assert True
+
+def assert_false():     #false会响应直接报错
+    assert False
+
+def assert_demo():      #a里面没有7所以报错
+    a="123456"
+    assert '7' in a
+
+def assert_demo2():     #a里面有6所以不报错
+    a="123456"
+    assert '6' in a
+
+def assert_demo3():     #报错
+    a="123456"
+    b=None
+    print(a+b)
+
+def assert_demo4():     #用try语句即使报错但依旧输出结果
+    a="123456"
+    try:
+        assert '7' in a
+    except:
+        print('a里面没有7')
+    print('-------')
+
+def assert_demo5():     #try之内的正确就不会输出except内的内容
+    a="123456"
+    try:
+        assert '6' in a
+    except:
+        print('a里面没有7')
+    print('-------')
+
+if __name__ == '__main__':
+    assert_demo4()
+
+
+
+
+
+
+
+
