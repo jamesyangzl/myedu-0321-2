2f7c19f2e7e1d9803083b97a7774c5a1e7f82042
杨自立 <1574746368@qq.com>
2019-03-27 11:21:43 +0800

    0326 2-46

--- day05/class_demo.py
+++ day05/class_demo.py
@@ -1,8 +1,10 @@
 # 声明类 class 声明方法 def
 # class 类
 # object 对象 / 或者所有父类
+# 面向对象编程
 
 # 声明类的语法：class：就是声明一个类；Human：类的名字（）：括号里面填 这个类的父类
+# class Human: （不写的话就自动继承object类）
 class Human(object):
 
     # 类的初始化方法 self：就代表类 本身
@@ -16,8 +18,32 @@ class Human(object):
     def myInfo(self):
         print('我叫%s，我今年%s岁，%s'%(self.name, self.age, self.sex))
 
+    def myInfoStr(self):
+        return '我叫%s，我今年%s岁，%s'%(self.name, self.age, self.sex)
+
+# 声明一个测试者 类，继承 Human类
+class Tester(Human):
+    # 声明了一个方法
+    def zhiXingCeShi(self):
+        # 调用了父亲的属性
+        print(self.name)
+        print('我在执行测试，别打扰我')
 
 if __name__ == '__main__':
-    human = Human('ysl',25,'男')
-    human.myInfo()
+    # = 后面 调用这个类 传入了 初始化的参数，参数必须传，而且传完整
+    # = 前面 是 对象名
+    # 对象 是 类的 实例化， 对象是变量的一种（类到对象，就是实例化的过程）
+    # human = Human('ysl',25,'男')
+    # print(type(human))
+    # # 对象可使用 类中的方法
+    # human.myInfo()
+    # info_str = human.myInfoStr()
+    # print(human.myInfoStr())
+    # print(info_str)
+
+
+    # 继承就是多态的一种表现方式，新建对象
+    tester = Tester('ysl',25,'男')
+    # 对象可使用 类中的方法
+    tester.zhiXingCeShi()
 
